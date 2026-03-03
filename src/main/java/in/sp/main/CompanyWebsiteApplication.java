276169f6851440659f1b982daffcf3928a11dcfd
1myheaven <abhisheksingh09072001@gmail.com>
2026-03-03 19:02:58 +0530

    error find import

--- src/main/java/in/sp/main/CompanyWebsiteApplication.java
+++ src/main/java/in/sp/main/CompanyWebsiteApplication.java
@@ -4,11 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableAsync
 public class CompanyWebsiteApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CompanyWebsiteApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(CompanyWebsiteApplication.class, args);
+    }
 }
--- src/main/java/in/sp/main/controller/publicsite/ConsultationController.java
+++ src/main/java/in/sp/main/controller/publicsite/ConsultationController.java
@@ -1,17 +1,38 @@
+package in.sp.main.controller.publicsite;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import in.sp.main.entity.ConsultationRequest;
+import in.sp.main.repository.ConsultationRepository;
+
 @Controller
+@RequestMapping("/consultation")
 public class ConsultationController {
-    
-//    @Autowired
-//    private EmailService emailService; // Service use karein
+
+    private final ConsultationRepository repository;
+
+    public ConsultationController(ConsultationRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping
+    public String showForm(Model model) {
+        model.addAttribute("consultationRequest", new ConsultationRequest());
+        return "layout/consultancy";
+    }
 
     @PostMapping
-    public String submitForm(@ModelAttribute ConsultationRequest request, Model model) {
-        repository.save(request);
-        
-        // Ye line ab turant execute ho jayegi, wait nahi karegi
-//        emailService.sendEmailToAdmin(request); 
+    public String submitForm(@ModelAttribute ConsultationRequest consultationRequest, Model model) {
+        // Database mein save karein
+        repository.save(consultationRequest);
 
-        model.addAttribute("successMessage", "Submitted!");
+        model.addAttribute("successMessage", "Request submitted! We will contact you soon.");
+        model.addAttribute("consultationRequest", new ConsultationRequest());
+
         return "layout/consultancy";
     }
 }
